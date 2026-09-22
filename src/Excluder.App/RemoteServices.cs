9806b096a292c8e2126a2b498ebfad35650312e7
143kmh <surzhanyevhen@gmail.com>
2026-09-22 22:24:29 +0300

    Use renamed GitHub repository for updates

--- src/Excluder.App/RemoteServices.cs
+++ src/Excluder.App/RemoteServices.cs
@@ -8,7 +8,7 @@ namespace Excluder.App;
 
 public static class RemoteServices
 {
-    public const string Repository = "143kmh/143-Overwatch-Finlad-Server-Excluder";
+    public const string Repository = "143kmh/143-OW-Switch";
     public const string CurrentVersion = "1.1.0";
     private static readonly HttpClient Client = CreateClient();
     private static HttpClient CreateClient()
